9dcde29decaeb120671f7b8438b82dcd6cc25a5b
lilyhoratio <lilyhzhou@gmail.com>
2019-06-21 11:05:06 -0700

    started creating Carousel class constructor and testing out modified styling in Carousel.less

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,24 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel
+        this.leftButton = this.carousel.querySelector(".left-button")
+        this.rightButton = this.carousel.querySelector(".right-button")
+        this.carouselImages = this.carousel.querySelectorAll(".carousel img")
+        // console.log(this.carouselImages);
 
+    }
+    //Methods
+    selectLeft() {
+
+    }
+
+    selectRight() {
+        
+    }
 }
 
-let carousel = document.querySelector();
-
+const carousel = new Carousel(document.querySelector(".carousel"))
+// new Carousel(carousel))
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
@@ -12,3 +27,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
