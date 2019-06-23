3810249d00b2ac5958af4459d8c65b63d665a9f7
lilyhoratio <lilyhzhou@gmail.com>
2019-06-22 22:14:36 -0700

    added keydown event listener for carousel image switching

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,63 +4,65 @@ class Carousel {
         this.leftButton = this.carousel.querySelector(".left-button")
         this.rightButton = this.carousel.querySelector(".right-button")
         this.carouselImages = this.carousel.querySelectorAll(".carousel img")
-        this.index = 0
+        this.currentIndex = 0
         this.lastIndex = this.carouselImages.length - 1
 
-            // 1st way - Set the first image's styling to display = "block"
-        this.carouselImages[this.index].style.display = "block"
+            // style.display approach
+        this.carouselImages[this.currentIndex].style.display = "block"
 
-            // 2nd way - why isn't adding the "img-show" class working?
+            // classList approach - why isn't adding the "img-show" class working?
         // console.log(this.carouselImages[this.index].classList.add("img-show"))
-        // console.log(this.carouselImages[this.index].classList)
 
-        this.rightButton.addEventListener("click", event => this.loopRight(event));
-        this.leftButton.addEventListener("click", event => this.loopLeft(event));
+        // How to trigger this only if user has clicked on carousel element first?
+        window.addEventListener("keydown", (event) => {           
+            if (event.key == "ArrowLeft") {
+                console.log(event)
+                this.loopLeft(event)
+            }
+            if (event.key == "ArrowRight") {
+                this.loopRight(event)
+            }
+        });
+
+        this.rightButton.addEventListener("click", () => this.loopRight())
+        this.leftButton.addEventListener("click", () => this.loopLeft())
     }
     loopRight(){ 
-        // When invoked, set all images to display="none", add one to index to select next image
-        // and set next image to display="block"
-
-        // console.log("here!")
-
-            // Set all images to none display
-        // document.querySelectorAll(".carousel img").forEach(img => img.style.display = "none")
+        // When invoked, 1) set all images to display="none", 
+        // 2) add one to index to select next image
+        // 3) and set next image to display="block"
     
-            // 1st way - 
-        // this.carouselImages.forEach(img => img.classList.toggle("img-show")) // remove? add .img-show if it doens't exist, remove if it does exist
+            // classList approach
+        // this.carouselImages.forEach(img => img.classList.remove("img-show"))
         
-            // 2nd way - 
+            // style.display approach
         Array.from(this.carouselImages).forEach(img => img.style.display = "none")
-
+        // document.querySelectorAll(".carousel img").forEach(img => img.style.display = "none") // better to use this.carouselImages or reselect from document?
         
-        if (this.index < this.lastIndex) {
+        if (this.currentIndex < this.lastIndex) {
             // Add one to the index, until the last image
-            this.index += 1
-        } else if (this.index = this.lastIndex) { 
+            this.currentIndex += 1
+        } else if (this.currentIndex == this.lastIndex) { // a=12 returns 12 // truthy
             // When current index is last index, restart index to 0
-            this.index = 0;
+            this.currentIndex = 0;
         }
 
-        this.carouselImages[this.index].style.display = "block"
+        this.carouselImages[this.currentIndex].style.display = "block"
     }
+
     loopLeft(){
-        console.log("here!")
-        let imageArr = Array.from(this.carouselImages)
-        console.log(imageArr);
-        // console.log(imageArr.forEach(img => img.src)) // what is the best way to find available methods on element to find img src?
-
-        console.log("ARRAY", Array.from(this.carouselImages).reverse());
-
-        // added reverse() to iterate backwards from the carouselImages array (start at last image?)
-        // Array.from(this.carouselImages).reverse().forEach(img => img.style.display = "none")
-        
-        // this.index += 1
-        // this.carouselImages[this.index].style.display = "block"
+        Array.from(this.carouselImages).forEach(img => img.style.display = "none")
+        if (this.currentIndex > 0) {
+            this.currentIndex -= 1
+        } else if (this.currentIndex == 0) {  
+            this.currentIndex = this.lastIndex
+        }
+        this.carouselImages[this.currentIndex].style.display = "block"
     }
 }
 
 const carousel = new Carousel(document.querySelector(".carousel"))
-// new Carousel(carousel))
+
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
